7d8f7dcb4019965957457b07ae80c4c8bec48f40
Abubeker Abduljelil <114282920+corax58@users.noreply.github.com>
2024-09-03 12:10:34 +0300

    added searching functionality

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Glow from "../components/Glow";
-import Fitlers from "./components/Fitlers";
 import { ProjectsList } from "@/data";
 import ProjectCard from "../components/ProjectCard";
 
 const page = () => {
+  const [search, setSearch] = useState<string>("");
+  const filteredList = ProjectsList.filter(
+    (item) =>
+      item.Title.toLowerCase().includes(search) ||
+      item.Title.toLowerCase().includes(search)
+  );
+
   return (
     <div className="w-screen">
       <div className="-z-50 absolute  -ml-40 -mt-40 ">
@@ -18,11 +25,14 @@ const page = () => {
         <input
           placeholder="Seach projects"
           className=" bg-primary border-2 border-secondary  outline-none focus:bg-third transition-all duration-300 py-2 px-4 text-white w-1/2 rounded-full"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
         />
       </div>
       <div className=" pt-12  px-10 lg:px-16 xl:px-32">
         <div className="  grid grid-cols-1 md:grid-cols-2 gap-5  justify-between w-full ">
-          {ProjectsList.map((Project) => (
+          {filteredList.map((Project) => (
             <div className="flex w-full justify-center" key={Project.id}>
               <ProjectCard Project={Project} />
             </div>
