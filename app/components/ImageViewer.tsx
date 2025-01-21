c5dbfa2ae8bdf44ccaafb3746211bcc19929be44
Abubeker Abduljelil <abubekera87@gmail.com>
2025-01-21 09:49:51 +0300

    Add Image viewer

--- app/components/ImageViewer.tsx
+++ app/components/ImageViewer.tsx
@@ -1,20 +1,53 @@
 "use client";
-import useEmblaCarousel from "embla-carousel-react";
-
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 interface Props {
   images: string[];
 }
 const ImageViewer = ({ images }: Props) => {
-  const [emblaRef] = useEmblaCarousel();
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="embla text-white" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide">Slide 1</div>
-        <div className="embla__slide">Slide 2</div>
-        <div className="embla__slide">Slide 3</div>
+    <>
+      <div className="bg-black ">
+        <Carousel className="w-full relative h-min  ">
+          <CarouselContent>
+            {images?.map((image) => (
+              <CarouselItem>
+                <img
+                  src={image}
+                  className=" h-full  md:max-h-96 lg:max-h-[450px]   w-full object-contain"
+                  onClick={() => setOpen(true)}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
-    </div>
+
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        plugins={[Zoom]}
+        slides={images.map((image) => ({
+          src: image,
+          alt: "image 1",
+          width: 3840,
+          height: 2560,
+        }))}
+      />
+    </>
   );
 };
 
--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,39 +1,17 @@
 import { ProjectsList } from "@/data";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import Glow from "@/app/components/Glow";
+
+import ImageViewer from "@/app/components/ImageViewer";
 
 const ProjectPage = ({ params }: { params: { projectId: string } }) => {
   var id = parseInt(params.projectId);
   let project = ProjectsList[id];
   if (!project) notFound();
-  console.log(project.Title);
   return (
     <div>
       <div className=" py-20  flex flex-col gap-10 h-max">
-        <div className="bg-black ">
-          <Carousel className="w-full relative h-min  ">
-            <CarouselContent>
-              {project.Images?.map((image) => (
-                <CarouselItem>
-                  <img
-                    src={image}
-                    className=" h-full  md:max-h-96 lg:max-h-[450px]   w-full object-contain"
-                  />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
+        <ImageViewer images={project.Images} />
         <div className="prose max-w-none   text-white px-5 md:px-20 font-light  overflow-clip ">
           <ReactMarkdown>{project.Details}</ReactMarkdown>
         </div>
