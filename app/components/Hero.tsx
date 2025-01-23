3cd8c612b8f150fa72ca2dd03c4e5c2c00e73a9a
Abubeker Abduljelil <114282920+corax58@users.noreply.github.com>
2025-01-23 11:27:06 +0300

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -24,12 +24,12 @@ const Hero = () => {
             craft innovative, user-friendly web solutions with expertise in both
             front-end and back-end technologies.
           </p>
-         <Link
+         <a target="_blank" 
     href="https://www.resume.lol/share/1n03vtm"
-    passHref
+   
     className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-md p-px text-xs font-semibold leading-6  text-white inline-block"
   >
-    <a target="_blank">
+    
       <span className="absolute inset-0 overflow-hidden rounded-md">
         <span className="absolute inset-0 rounded-md bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
       </span>
@@ -38,7 +38,7 @@ const Hero = () => {
       </div>
       <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
     </a>
-  </Link>
+ 
         </div>
         <div className=" flex items-center justify-center">
           <div className="rounded-full overflow-hidden border-[10px] lg:border-[18px] border-[#323571] size-56 lg:size-72 xl:size-96 ">
